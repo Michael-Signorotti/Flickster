9f137b0804b1c99dc699a0aa421af4737ab93c7f
Michael Signorotti <michaelsignorotti@Michaels-MacBook-Pro.local>
2017-09-15 17:50:41 -0700

    Completed basic functionality

---
+++ app/src/main/java/com/codepath/flickster/models/Movie.java
@@ -0,0 +1,53 @@
+package com.codepath.flickster.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by michaelsignorotti on 9/13/17.
+ */
+
+public class Movie {
+
+
+    private String posterPath;
+    private String originalTitle;
+    private String overview;
+
+    public Movie(JSONObject jsonObject) throws JSONException {
+        this.posterPath = jsonObject.getString("poster_path");
+        this.originalTitle = jsonObject.getString("original_title");
+        this.overview = jsonObject.getString("overview");
+    }
+
+    public static ArrayList<Movie> fromJSONArray(JSONArray array) {
+        ArrayList<Movie> movieList = new ArrayList<Movie>();
+
+        for (int i = 0; i < array.length(); i++) {
+            Movie m = null;
+            try {
+                m = new Movie(array.getJSONObject(i));
+                movieList.add(m);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return movieList;
+    }
+
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+}
